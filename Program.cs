a0b7f5785b577e2afb0cebe84f7289e7bb8cf2c5
Gergő Horváth <horvathgergo1208@gmail.com>
2020-09-30 11:55:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//BigInteger osztály (struktúra) elérése:
+//System.numerics hozzáadása a referenciákhoz
+using System;
+using System.Numerics;
+
+namespace faktoriálisBigInteger
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Faktoriális meghatározása 20-40 között");
+            for (int szám = 20; szám <= 1000; szám++)
+            {
+                BigInteger faktor = 1;
+                for (int i = 2; i <= szám; i++)
+                {
+                    faktor = faktor * i;
+                }
+                Console.WriteLine($"{szám}!={faktor}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
